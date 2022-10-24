2dadee03c8b68a027fa428dccf02e549bc850c91
winniechae1 <chaewon@i2max.co.kr>
2022-10-24 13:45:53 +0900

    ch06-6 encapsulation variable

--- ch6/6-6-use.js
+++ ch6/6-6-use.js
@@ -1,6 +1,6 @@
 import { getDefaultOwner } from "./6-6.js";
 
 const owner = getDefaultOwner();
-// owner.firstName = "엘리";
+// owner.firstName = "엘리"; // set이 안됨
 console.log(owner.firstName);
 console.log(getDefaultOwner());
