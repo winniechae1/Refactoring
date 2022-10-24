e910e9d3a927b161351274296fe1b841675bcfb8
winniechae1 <chaewon@i2max.co.kr>
2022-10-24 10:43:11 +0900

    ch06-3 extract variable

--- ch6/6-3-2.js
+++ ch6/6-3-2.js
@@ -1,20 +1,28 @@
 export class Order {
+  #data; // _data 대신에 #data로 사용 (private data인 경우 사용)
   constructor(aRecord) {
-    this._data = aRecord;
+    this.#data = aRecord;
   }
 
   get quantity() {
-    return this._data.quantity;
+    return this.#data.quantity;
   }
   get itemPrice() {
-    return this._data.itemPrice;
+    return this.#data.itemPrice;
   }
 
   get price() {
-    return (
-      this.quantity * this.itemPrice -
-      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-      Math.min(this.quantity * this.itemPrice * 0.1, 100)
-    );
+    return this.basePrice - this.discount + this.shipping;
+  }
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+
+  get discount() {
+    Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+
+  get shipping() {
+    Math.min(this.quantity * this.itemPrice * 0.1, 100);
   }
 }
