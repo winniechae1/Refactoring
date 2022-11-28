c50cd18b7ad25f0b0875051b9f4c5a046ed65438
winniechae1 <chaewon@i2max.co.kr>
2022-11-28 16:42:58 +0900

    ch07-05 extract class

--- ch7/7-5.js
+++ ch7/7-5.js
@@ -1,4 +1,4 @@
-class Person {
+/*class Person {
   #name;
   #officeAreaCode;
   #officeNumber;
@@ -35,10 +35,79 @@ class Person {
   set officeNumber(arg) {
     this.#officeNumber = arg;
   }
+}*/
+class Person {
+  #name;
+  #telephoneNumber;
+  constructor(name, areaCode, number) {
+    this.#name = name;
+    this.#telephoneNumber = new TelephoneNumber(areaCode, number);
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set name(arg) {
+    this.#name = arg;
+  }
+
+  get telephoneNumber() {
+    return this.#telephoneNumber.toString;
+  }
+  /*
+  get officeAreaCode() {
+    return this.#telephoneNumber.areaCode;
+  }
+
+  set officeAreaCode(arg) {
+    this.#officeAreaCode = arg;
+  }
+
+  get officeNumber() {
+    return this.#officeNumber;
+  }
+
+  set officeNumber(arg) {
+    this.#officeNumber = arg;
+  }*/
 }
 
-const person = new Person('엘리', '010', '12345678');
-console.log(person.name);
-console.log(person.officeAreaCode);
-console.log(person.officeNumber);
-console.log(person.telephoneNumber);
+class TelephoneNumber {
+  #areaCode;
+  #number;
+  constructor(area, number) {
+    this.#areaCode = area;
+    this.#number = number;
+  }
+
+  get areaCode() {
+    return this.#areaCode;
+  }
+
+  set areaCode(arg) {
+    this.#areaCode = arg;
+  }
+
+  get number() {
+    return this.#number;
+  }
+  set number(arg) {
+    this.#number = arg;
+  }
+
+  get toString() {
+    return `(${this.areaCode}) ${this.number}`;
+  }
+}
+
+const telephoneNumber = new TelephoneNumber("010", "12345678");
+telephoneNumber.toString;
+console.log(telephoneNumber.toString);
+console.log(telephoneNumber.areaCode);
+console.log(telephoneNumber.number);
+//const person = new Person("엘리", "010", "12345678");
+//console.log(person.name);
+//console.log(person.officeAreaCode);
+//console.log(person.officeNumber);
+//console.log(person.telephoneNumber);
