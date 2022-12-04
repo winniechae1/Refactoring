76642a0cff5a5ade823be363eb9766d80f921fbe
winniechae1 <kwakseok1034@gmail.com>
2022-12-04 23:52:38 +0900

    ch10 after half

--- ch10/10-2.js
+++ ch10/10-2.js
@@ -1,6 +1,18 @@
+/* 
+    동일한 동작을 수행하는 다양한 조건들이 있다면 그 조건들을 하나로 뭉친다음에 리팩토링 하자. 
+*/
+
 function disabilityAmount(employee) {
-  if (employee.seniority < 2) return 0;
-  if (employee.monthsDisabled > 12) return 0;
-  if (employee.isPartTime) return 0;
+  if (isNotEligibleForDisability(employee)) {
+    return 0;
+  }
   return 1;
+
+  function isNotEligibleForDisability(employee) {
+    return (
+      employee.seniority < 2 ||
+      employee.monthsDisabled > 12 ||
+      employee.isPartTime
+    );
+  }
 }
